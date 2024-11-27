3cf11ce06135f469e980505faf310336db7bcaec
LeAnne B. <branchleannelmb@gmail.com>
2024-11-27 13:01:05 -0500

    adds start of scoring

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -109,6 +109,20 @@ public class GamePanel extends JPanel implements Runnable{
 		if (paddle2.y >= (GAME_HEIGHT - PADDLE_HEIGHT))
 			paddle2.y= GAME_HEIGHT - PADDLE_HEIGHT;
 		
+		//scoring and new paddle/ball
+		if(ball.x <= 0) {
+			score.player2++;
+			newPaddles();
+			newBall();
+			System.out.println("Player 2: " + score.player2);
+		}
+		if(ball.x >= GAME_WIDTH-BALL_DIAMETER) {
+			score.player1++;
+			newPaddles();
+			newBall();
+			System.out.println("Player 1: " + score.player1);
+		}
+		
 	}
 	
 	public void run() {
